90aa43ec77ca3f536c76b3b641110bed312d5f36
Seif Hisham <74967136+seifhisham@users.noreply.github.com>
2025-08-26 20:10:33 +0300

    Update intro.tsx
    made it the path of the CV correctly

--- components/intro.tsx
+++ components/intro.tsx
@@ -74,7 +74,7 @@ export default function Intro() {
 
             <a className='group bg-white dark:bg-white/10 text-gray-900 dark:text-white/80 px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110
              active:scale-105 transition cursor-pointer border border-black/5 dark:border-white/5' 
-             href='/cv.pdf' 
+             href='/CV.pdf' 
              download
             >Download CV <HiDownload className='opacity-60 group-hover:translate-y-1 transition' /></a>
 
